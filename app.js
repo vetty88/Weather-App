5825bc82e47b7a08771f60a947e4dc3304ad7b45
Vetty <vetty88@gmail.com>
2020-06-25 20:41:54 +1000

    included jquery reference within main js file

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-$(document).ready (function () {
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ 
+ $(document).ready (function () {
     
     var city = $('#city-name').val ();
     var itemKey = today;
