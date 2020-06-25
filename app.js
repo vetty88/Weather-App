b32231935114ab69139f2709f6ead4178ea3c37e
Vetty <vetty88@gmail.com>
2020-06-25 20:19:34 +1000

    removed xmlHttprequest rule - seemed to cause conflicts within js files

--- app.js
+++ app.js
@@ -19,18 +19,6 @@ $(document).ready (function () {
             lon: forecastData.city.coord.lon,
         },
 
-    function loadDoc(url, cFunction) {
-        var xhttp;
-        xhttp=new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-            cFunction(this);
-            }
-        };
-        xhttp.open("GET", url, true);
-        xhttp.send();
-        },
-    
     $.when ($.ajax("xhttp://api.openweathermap.org/data/2.5/weather?q={city name}&appid="), 
     $.ajax("xhttp://api.openweathermap.org/data/2.5/forecast?q={city name}&appid=)"),
     $.ajax("xhttp://api.openweathermap.org/data/2.5/uvi?appid=${apiKey1}&lat=${lat}&lon=${lon}"))
